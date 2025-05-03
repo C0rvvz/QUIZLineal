66dbc3efd6cdfb0a297965e9c924fdaddb509036
Juan Diego Arango Molina <ajuandiego036@gmail.com>
2025-05-02 21:07:09 -0500

    Add compiled Python files to __pycache__ and clean up import statement in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from gui.app import GaussGameApp  # Correct import for the GaussGameApp class
+from gui.app import GaussGameApp 
 
 if __name__ == "__main__":
     root = tk.Tk()
